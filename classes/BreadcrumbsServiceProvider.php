89c56bccf75d8e413304f5bacabcc682f9e40867
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2020-04-21 09:05:21 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- classes/BreadcrumbsServiceProvider.php
+++ classes/BreadcrumbsServiceProvider.php
@@ -71,12 +71,12 @@ class BreadcrumbsServiceProvider extends ServiceProvider implements DeferrablePr
         // Load the routes/breadcrumbs.php file, or other configured file(s)
         $files = config('breadcrumbs.files');
 
-        if (! $files) {
+        if (!$files) {
             return;
         }
 
         // If it is set to the default value and that file doesn't exist, skip loading it rather than causing an error
-        if ($files === base_path('routes/breadcrumbs.php') && ! is_file($files)) {
+        if ($files === base_path('routes/breadcrumbs.php') && !is_file($files)) {
             return;
         }
 
