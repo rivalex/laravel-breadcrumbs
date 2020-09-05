eec40f9434692aa3db0b8ba4006ded5cda8f4e27
Alessandro <alessandro@rivalex.com>
2020-09-05 12:16:17 +0200

    Update bootstrap4icons.blade.php
    Add html support to breadcrumbs title

--- views/bootstrap4icons.blade.php
+++ views/bootstrap4icons.blade.php
@@ -6,12 +6,12 @@
             @if ($breadcrumb->url && !$loop->last)
                 <li class="breadcrumb-item">
                     <a href="{{ $breadcrumb->url }}">
-                        <i class="{{ $breadcrumb->faIcon ?? '' }} mr-2" aria-hidden="true"></i>{{ $breadcrumb->title }}
+                        <i class="{{ $breadcrumb->faIcon ?? '' }} mr-2" aria-hidden="true"></i>{!! $breadcrumb->title !!}
                     </a>
                 </li>
             @else
                 <li class="breadcrumb-item active">
-                    <i class="{{ $breadcrumb->faIcon ?? '' }} mr-2" aria-hidden="true"></i>{{ $breadcrumb->title }}
+                    <i class="{{ $breadcrumb->faIcon ?? '' }} mr-2" aria-hidden="true"></i>{!! $breadcrumb->title !!}
                 </li>
             @endif
 
