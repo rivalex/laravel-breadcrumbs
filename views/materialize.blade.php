f6c217f1b2f3de468b0b8af9923fdc8cd7884c28
Alessandro <alessandro@rivalex.com>
2020-09-05 12:19:29 +0200

    Update materialize.blade.php

--- views/materialize.blade.php
+++ views/materialize.blade.php
@@ -6,9 +6,9 @@
                 @foreach ($breadcrumbs as $breadcrumb)
 
                     @if ($breadcrumb->url && !$loop->last)
-                        <a href="{{ $breadcrumb->url }}" class="breadcrumb">{{ $breadcrumb->title }}</a>
+                        <a href="{{ $breadcrumb->url }}" class="breadcrumb">{!! $breadcrumb->title !!}</a>
                     @else
-                        <span class="breadcrumb">{{ $breadcrumb->title }}</span>
+                        <span class="breadcrumb">{!! $breadcrumb->title !!}</span>
                     @endif
 
                 @endforeach
